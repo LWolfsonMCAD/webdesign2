27af610e4ba6b76f6d1756f078bddf56e5dfb6d2
LWolfsonMCAD <lwolfson@mcad.edu>
2016-04-10 22:08:55 -0500

    Commit worked, but files wouldn't push...manually dumping them in.

--- cozycookie/public/js/jquery.js
+++ cozycookie/public/js/jquery.js
@@ -1,17 +1,48 @@
 $( document ).ready(function() {
 	var
-		
+		viewButtons = $("div.toggleButtons button"),
+		readableRecipe = $("div.recipeInstructions div.readableRecipe"),
+		videoRecipe = $("div.recipeInstructions div.videoRecipe"),
+		startingButton = $("div.toggleButtons button#readRecipeBtn"),
+		startingSection = $( readableRecipe )
 	;
 
 
 //Override default styles
+	$( startingButton ).addClass( "currentView" );
+
+	$( startingSection ).addClass( "currentSection" );
+
+	$( videoRecipe ).addClass( "inactive" );
 
 
+//Toggle recipe view based on user's choice.
+
+	$( viewButtons ).on('click', function() {
+		previousButton = $( "button.currentView" ),
+		previousSection = $( "div.currentSection" );
+
+		$( this ).toggleClass( "currentView" );
+
+		$( previousButton ).toggleClass( "currentView" );
+
+			$( videoRecipe )
+				.toggleClass( "inactive currentSection" );
+			$( readableRecipe )
+				.toggleClass( "currentSection inactive" );
+		
+	});
 
 
+//Popup for ProTip.
 
-//Main navigation actions.
-
+	$('span.info').mouseenter(function() {
+	      // $('img.hover-image').remove();
+	      $(this).append('<img class="hover-image" src="img/coolingrack_protip.jpg"/>')
+	    }).mouseleave(function() {
+	      $('img.hover-image').remove();
+	    })
+	
 
 
 
