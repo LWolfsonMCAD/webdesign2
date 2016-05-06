7bd98b55d1e595bceac8401dbbbd062369dca7cd
Lisa Myers <lwolfson@mcad.edu>
2016-05-05 22:34:19 -0500

    All four pages, responsive between desktop, tablet and mobile. Added a hint of jQuery, still have some tweaks needed.

--- moam_html_build/public/js/jquery.js
+++ moam_html_build/public/js/jquery.js
@@ -1,50 +1,27 @@
 $( document ).ready(function() {
 	var
-		viewButtons = $("div.toggleButtons button"),
-		readableRecipe = $("div.recipeInstructions div.readableRecipe"),
-		videoRecipe = $("div.recipeInstructions div.videoRecipe"),
-		startingButton = $("div.toggleButtons button#readRecipeBtn"),
-		startingSection = $( readableRecipe )
+		blocklink_info = $("div.block-link div.more-info"),
+		blocklink = $("div.block-link")
 	;
 
 
 //Override default styles
-	$( startingButton ).addClass( "currentView" );
-
-	$( startingSection ).addClass( "currentSection" );
-
-	$( videoRecipe ).addClass( "inactive" );
+	$( blocklink_info ).hide();
 
 
-//Toggle recipe view based on user's choice.
+	$( blocklink ).on ({
+		mouseenter: function() {
+			$( this )
+				.find( blocklink_info )
+				.slideDown(600);
+		},
 
-	$( viewButtons ).on('click', function() {
-		previousButton = $( "button.currentView" ),
-		previousSection = $( "div.currentSection" );
-
-		$( this ).toggleClass( "currentView" );
-
-		$( previousButton ).toggleClass( "currentView" );
-
-			$( videoRecipe )
-				.toggleClass( "inactive currentSection" );
-			$( readableRecipe )
-				.toggleClass( "currentSection inactive" );
-		
+		mouseleave: function() {
+			$( this )
+				.find( blocklink_info )
+				.slideUp(600);
+		}
 	});
-
-
-//Popup for ProTip.
-
-	$('span.info').mouseenter(function() {
-	      // $('img.hover-image').remove();
-	      $(this).append('<img class="hover-image" src="img/coolingrack_protip.jpg"/>')
-	    }).mouseleave(function() {
-	      $('img.hover-image').remove();
-	    })
-	
-
-
 
 });
 
