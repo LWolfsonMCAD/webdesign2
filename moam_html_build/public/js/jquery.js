0cf57dc30ede5a1ae069daf78751c2e6bc3c7f06
Lisa Myers <lwolfson@mcad.edu>
2016-05-08 00:15:28 -0500

    Polished everything up. Finished up mobile styles/menu. Tweaked the jQuery a bit too.

--- moam_html_build/public/js/jquery.js
+++ moam_html_build/public/js/jquery.js
@@ -1,27 +1,68 @@
 $( document ).ready(function() {
 	var
+		blocklink_section = $("div.site-sections"),
 		blocklink_info = $("div.block-link div.more-info"),
-		blocklink = $("div.block-link")
+		blocklink = $("div.block-link"),
+		desktop_blocklinks = $("div.site-sections.jsBlocks"),
+		mobileNav = $("div#mobile-navigation")
 	;
 
 
+
+
+
 //Override default styles
-	$( blocklink_info ).hide();
+    $( blocklink_info ).hide();
+	
+
+	//Mobile Menu
+	$("img#hamburger_icon").on('click', function(){
+		$( "div#mobile-navigation" ).toggle();
+	});	
+
+    // run test on initial page load
+    checkSize();
+
+    // run test on resize of the window
+    $(window).resize(checkSize);
 
 
-	$( blocklink ).on ({
-		mouseenter: function() {
-			$( this )
-				.find( blocklink_info )
-				.slideDown(600);
-		},
+	//Function to the css rule
+	function checkSize(){
+    	if ($( "img#hamburger_icon" ).css("display") == "block" ){
+    		$( blocklink_section ).removeClass( "jsBlocks" );
+    		$( blocklink_info ).show();
 
-		mouseleave: function() {
-			$( this )
-				.find( blocklink_info )
-				.slideUp(600);
-		}
-	});
+    	}
+
+    	if ($( "img#hamburger_icon" ).css("display") == "none" ){
+    		$( blocklink_section ).addClass( "jsBlocks" );
+    		$( blocklink_info ).hide();
+    		$( mobileNav ).hide();
+    		desktopSiteSectionHover();
+
+    	}
+
+	}
+
+
+	function desktopSiteSectionHover() {
+		$( blocklink ).on ({
+			mouseenter: function() {
+				$( this )
+					.find( blocklink_info )
+					.slideDown(600);
+			},
+
+			mouseleave: function() {
+				$( this )
+					.find( blocklink_info )
+					.slideUp(600);
+			}
+		});
+
+	}
+
 
 });
 
